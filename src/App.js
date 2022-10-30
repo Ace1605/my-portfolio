682927c0fca1baf30d73983212b5d1e6f6f9933c
Ace1605 <46580446+Ace1605@users.noreply.github.com>
2022-10-30 21:44:57 +0100

    Link wrapper creation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import './App.css';
 import styled from "styled-components";
 import { ReactComponent as BackIcon } from "./svg/back-button.svg";
 import { ReactComponent as OptionsIcon } from "./svg/three-dot.svg";
+import { ReactComponent as SlackIcon } from "./svg/slack.svg";
+import { ReactComponent as GitIcon } from "./svg/github.svg";
+import { ReactComponent as ZuriIcon } from "./svg/zuri-logo.svg";
+import { ReactComponent as IFGIcon } from "./svg/ifg.svg";
 
+
+
+const LinkWrapper = styled.div`
+  position: relative;
+  margin: 0 auto;
+  padding: 1rem 2rem 2rem 2rem;
+  max-width: 82.28rem;
+
+`;
 
 
 
@@ -24,6 +37,26 @@ main.style.filter = 'blur(0px)'
             <h1 id='twitter'>Achugo Ebuka</h1>
             <h2 id='slack'>AceDev</h2>
         </header>
+        <LinkWrapper>
+                <a href="https://twitter.com/AchugoEbuka" target="_blank" rel='noreferrer'>
+                    Twitter Link
+                </a>
+                <a id='btn__zuri' href="https://training.zuri.team/"  target="_blank" rel='noreferrer'>
+                    Zuri Team
+                </a>
+                <a id='books' href="http://books.zuri.team " target="_blank" rel='noreferrer' title='Click here to access your design and coding books.' >
+                    Zuri Books
+                </a>
+                <a id='book__python' title='Your Python adventure begins with just a click!' href="https://books.zuri.team/python-for-beginners?ref_id=AceDev" target="_blank" rel='noreferrer' >
+                    Python Books
+                </a>
+                <a id='pitch' href="https://background.zuri.team" target="_blank" rel='noreferrer' title='Visit this page for exclusive information on coders'>
+                    Background Check for Coders
+                </a>
+                <a id='book__design' title='Don&apos;t miss out. Get Zuri design books absolutely FREE!!! ' href="https://books.zuri.team/design-rules" target="_blank" rel='noreferrer' >
+                    Design Books
+                </a>
+        </LinkWrapper>
       </main>
     </div>
   );
