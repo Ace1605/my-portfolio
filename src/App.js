29e5b99359e02d033219ab76ceae6f22d0301bb4
Ace1605 <46580446+Ace1605@users.noreply.github.com>
2022-10-30 21:35:55 +0100

    first commit for zuri basic webpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import { ReactComponent as BackIcon } from "./svg/back-button.svg";
+import { ReactComponent as OptionsIcon } from "./svg/three-dot.svg";
+
+
+
 
 function App() {
+
+  setTimeout(() => {
+    const main = document.querySelector('main')
+main.style.opacity = 1
+main.style.filter = 'blur(0px)'
+}, 1000)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <header id='profile__section'>
+            <BackIcon className="back" />
+            <OptionsIcon className='dots'/>
+            <img src="/Ace.jpeg" alt="profile" id="profile__img" />
+            <h1 id='twitter'>Achugo Ebuka</h1>
+            <h2 id='slack'>AceDev</h2>
+        </header>
+      </main>
     </div>
   );
 }
